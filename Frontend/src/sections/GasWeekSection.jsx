00add7366a72142024b8b0a19af4b1efb0cc95d2
Shin <shinyakoike3564@gmail.com>
2026-08-12 09:45:44 +0800

    starting on weekly gas section

---
+++ Frontend/src/sections/GasWeekSection.jsx
@@ -0,0 +1,13 @@
+
+const GasWeekSection = () => {
+    return (
+        <section>
+            <div className="relative mx-auto  max-w-6xl flex-col gap-8 lg:flex-row lg:items-center lg:justify-between ">
+            <p className="text-emerald-600 text-xs font-semibold tracking-widest mb-3">THIS WEEK'S PRICES</p>
+            <h2 className="text-white text-3xl font-bold mb-2">DOE reference prices</h2>
+        </div>
+        </section>
+    )
+}
+
+export default GasWeekSection;
