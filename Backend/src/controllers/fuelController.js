894fb8cdb48571fb6603fb2139ca3f4994381147
Shin <shinyakoike3564@gmail.com>
2026-08-14 21:19:41 +0800

    refactor: change update for previous price | change %

--- Backend/src/controllers/fuelController.js
+++ Backend/src/controllers/fuelController.js
@@ -1,24 +1,27 @@
-const FuelPrice = require('../models/FuelPrice')
-
-// GET 
-const getAllFuelPrices = async (req, res) => {
-  try {
-    const prices = await FuelPrice.find()
-    res.json(prices)
-  } catch (err) {
-    res.status(500).json({ message: err.message })
-  }
-}
-
-// UPDATE 
 const updateFuelPrice = async (req, res) => {
   try {
     const { id } = req.params
-    const updated = await FuelPrice.findByIdAndUpdate(id, req.body, { new: true })
+    const { price } = req.body
+
+    // get current price first
+    const current = await FuelPrice.findById(id)
+
+    // calculate change automatically
+    const change = price - current.price
+
+    const updated = await FuelPrice.findByIdAndUpdate(
+      id,
+      {
+        previousPrice: current.price, // save old price
+        price: price,                 // set new price
+        change: change,               // auto calculated
+        updatedAt: Date.now()
+      },
+      { new: true }
+    )
+
     res.json(updated)
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
 }
-
-module.exports = { getAllFuelPrices, updateFuelPrice }
