eceb719e4a8874196a6fb8cf177f8e1c19d0e99c
Shin <shinyakoike3564@gmail.com>
2026-08-16 19:33:00 +0800

    route admin fuel page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,42 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import './App.css'
-import HeroSection from './sections/HeroSection';
-import InfoSection from './sections/InfoSection';
-import Steps from './sections/Steps';
-import GasWeekSection from './sections/GasWeekSection';
+import HeroSection from './sections/HeroSection'
+import InfoSection from './sections/InfoSection'
+import Steps from './sections/Steps'
+import GasWeekSection from './sections/GasWeekSection'
+import AdminSection from './sections/AdminSection'
+
+const MainPage = () => (
+  <>
+    <div className="pt-40">
+
+      <Navbar />
+      <HeroSection />
+      <InfoSection />
+      <Steps />
+      <GasWeekSection />
+    </div>
+  </>
+)
+
+const FuelDataPage = () => (
+  <>
+    <div className="pt-40">
+      <Navbar />
+      <AdminSection />
+    </div>
+  </>
+)
 
 function App() {
   return (
-    <>
-      <div className="pt-40">
-        <Navbar />
-        <HeroSection />
-        <InfoSection />
-        <Steps />
-        <GasWeekSection />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/fueldata" element={<FuelDataPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App
